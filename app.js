25fc6fef2ccf3137521c0e8e5afc1e441deed7f5
Carlos Moreno <wolffydev@gmail.com>
2023-07-06 10:42:15 -0600

    new version to try to connect with vercel

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const path = require('path')
 
 app.use(express.static(path.join(__dirname + '/public')))
 
-app.listen(3000,()=>{
+// here you need to change the ip for yoyr own localhost ip to be able to see the web on your other devices ,"192.168.100.2" example
+app.listen(3000,()=>{ 
     console.log('Server running on port', 3000)
 })
 
@@ -13,6 +14,3 @@ app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname + '/public/html/home.html'))
 })
 
-app.get('/portfolio',(req,res)=>{
-    res.sendFile(path.join(__dirname + '/public/html/freecode.html'))
-})
